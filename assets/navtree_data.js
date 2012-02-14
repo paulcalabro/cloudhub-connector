4d9ccba9d7445dc35248b1b29108e20c48c9c0cd
MuleSoft <code-contrib@mulesoft.com>
2012-02-13 16:06:19 -0800

    Updating documentation

---
+++ assets/navtree_data.js
@@ -0,0 +1,11 @@
+var NAVTREE_DATA =
+[ [ "org.mule.modules.ion", "org/mule/modules/ion/package-summary.html", [ [ "Classes", null, [ [ "IONConnector", "org/mule/modules/ion/IONConnector.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.modules.ion.config", "org/mule/modules/ion/config/package-summary.html", [ [ "Classes", null, [ [ "DeployMessageProcessor", "org/mule/modules/ion/config/DeployMessageProcessor.html", null, "" ], [ "IONConnectorCapabilitiesAdapter", "org/mule/modules/ion/config/IONConnectorCapabilitiesAdapter.html", null, "" ], [ "IONConnectorLifecycleAdapter", "org/mule/modules/ion/config/IONConnectorLifecycleAdapter.html", null, "" ], [ "UndeployMessageProcessor", "org/mule/modules/ion/config/UndeployMessageProcessor.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.modules.ion.config.spring", "org/mule/modules/ion/config/spring/package-summary.html", [ [ "Classes", null, [ [ "DeployDefinitionParser", "org/mule/modules/ion/config/spring/DeployDefinitionParser.html", null, "" ], [ "IONConnectorConfigDefinitionParser", "org/mule/modules/ion/config/spring/IONConnectorConfigDefinitionParser.html", null, "" ], [ "IONConnectorNamespaceHandler", "org/mule/modules/ion/config/spring/IONConnectorNamespaceHandler.html", null, "" ], [ "UndeployDefinitionParser", "org/mule/modules/ion/config/spring/UndeployDefinitionParser.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ java/lists.js
@@ -0,0 +1,17 @@
+var DATA = [
+      { id:0, label:"deploy", link:"mule/ion.html#deploy", type:"method" },
+      { id:1, label:"org.mule.modules.ion", link:"java/org/mule/modules/ion/package-summary.html", type:"package" },
+      { id:2, label:"org.mule.modules.ion.IONConnector", link:"java/org/mule/modules/ion/IONConnector.html", type:"class" },
+      { id:3, label:"org.mule.modules.ion.config", link:"java/org/mule/modules/ion/config/package-summary.html", type:"package" },
+      { id:4, label:"org.mule.modules.ion.config.DeployMessageProcessor", link:"java/org/mule/modules/ion/config/DeployMessageProcessor.html", type:"class" },
+      { id:5, label:"org.mule.modules.ion.config.IONConnectorCapabilitiesAdapter", link:"java/org/mule/modules/ion/config/IONConnectorCapabilitiesAdapter.html", type:"class" },
+      { id:6, label:"org.mule.modules.ion.config.IONConnectorLifecycleAdapter", link:"java/org/mule/modules/ion/config/IONConnectorLifecycleAdapter.html", type:"class" },
+      { id:7, label:"org.mule.modules.ion.config.UndeployMessageProcessor", link:"java/org/mule/modules/ion/config/UndeployMessageProcessor.html", type:"class" },
+      { id:8, label:"org.mule.modules.ion.config.spring", link:"java/org/mule/modules/ion/config/spring/package-summary.html", type:"package" },
+      { id:9, label:"org.mule.modules.ion.config.spring.DeployDefinitionParser", link:"java/org/mule/modules/ion/config/spring/DeployDefinitionParser.html", type:"class" },
+      { id:10, label:"org.mule.modules.ion.config.spring.IONConnectorConfigDefinitionParser", link:"java/org/mule/modules/ion/config/spring/IONConnectorConfigDefinitionParser.html", type:"class" },
+      { id:11, label:"org.mule.modules.ion.config.spring.IONConnectorNamespaceHandler", link:"java/org/mule/modules/ion/config/spring/IONConnectorNamespaceHandler.html", type:"class" },
+      { id:12, label:"org.mule.modules.ion.config.spring.UndeployDefinitionParser", link:"java/org/mule/modules/ion/config/spring/UndeployDefinitionParser.html", type:"class" },
+      { id:13, label:"undeploy", link:"mule/ion.html#undeploy", type:"method" }
+
+    ];
